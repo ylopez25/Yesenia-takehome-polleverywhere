03c4ee8a250c9246070c5431f04f64393cab8cf6
Yesenia L <ylopezcr123@gmail.com>
2023-01-14 10:53:03 -0500

    created show one raffle and create raffle route

--- controllers/rafflesController.js
+++ controllers/rafflesController.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const raffles = express.Router();
-const { getAllRaffles } = require('../queries/raffles.js')
+const { getAllRaffles, getRaffle, newRaffle } = require('../queries/raffles.js')
 
 raffles.get("/",  async (req,res) => {
     const allRaffles = await getAllRaffles();
     res.json(allRaffles)
 });
+
+//SHOW one
+raffles.get("/:id", async (req,res) => {
+    const {id} = req.params;
+    try {
+        const raffle = await getRaffle(id);
+        if(raffle["id"]) {
+            res.json(raffle);
+        }else {
+            console.log(`Database rror: ${raffle}`)
+            throw `No existing raffle ${id}!`
+        }
+    }catch (err) {
+        res.status(404).json({error: 'Resource not found.', message: err})
+    }
+});
+
+//CREATE
+raffles.post("/", async (req,res) => {
+    try {
+    const raffle = await newRaffle(req.body);
+    if (raffle['id']) {
+        res.json(raffle);
+    }else {
+        console.log(`Database error: ${raffle}`);
+        throw `Error dding ${req.body} to the database.`;
+    }
+    }catch (err) {
+        res.status(404).json({err:err})
+    }
+})
 
 module.exports = raffles;
--- queries/raffles.js
+++ queries/raffles.js
@@ -1,14 +1,40 @@
 const db = require("../db/dbConfig.js");
 
 const getAllRaffles = async () => {
-    try{
-        const allRaffles = await db.any('SELECT * FROM raffles');
-        return allRaffles;
+  try {
+    const allRaffles = await db.any("SELECT * FROM raffles");
+    return allRaffles;
+  } catch (err) {
+    return err;
+  }
+};
+
+//SHOW ONE
+const getRaffle = async (id) => {
+  try {
+    const oneRaffle = await db.one("SELECT * FROM raffles WHERE id=$1", id);
+    return oneRaffle;
+  } catch (err) {
+    return err;
+  }
+};
+
+//CREATE
+const newRaffle = async (raffle) => {
+    try {
+        //throw more error handling and validation
+        const createRaffle = await db.one(
+            'INSERT INTO raffles (rafflename, date, is_winner, secretcode) VALUES($1, $2, $3,$4) RETURNING *',
+            [raffle.rafflename, raffle.date, raffle.is_winner, raffle.secretcode]
+        )
+        return createRaffle;
     }catch (err) {
         return err
     }
-};
+}
 
-module.exports = { 
-    getAllRaffles
+module.exports = {
+  getAllRaffles,
+  getRaffle,
+  newRaffle,
 };
